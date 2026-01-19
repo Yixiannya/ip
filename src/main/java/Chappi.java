ca5ca38f19fe40a59ecd989f46229f3c89223737
Yixiannya <ohyixian@gmail.com>
2026-01-19 17:26:53 +0800

    Implemented basic input response

--- src/main/java/Chappi.java
+++ src/main/java/Chappi.java
@@ -1,11 +1,57 @@
+import java.util.Scanner;
+
 public class Chappi {
+    // Common use strings
+    private static String seperator = "     ____________________________________________________________\n";
+    private static Scanner scanner;
+
     public static void main(String[] args) {
         // Messages
-        String newline = "____________________________________________________________\n";
-        String greeting = " Hello! I'm Chappi!\n" +
-                " What can I do for you?\n";
-        String goodbye = " Bye. Hope to see you again soon!\n";
+        String greeting = "      Hello! I'm Chappi!\n" +
+                "      What can I do for you?\n";
 
-        System.out.println(newline + greeting + newline + goodbye + newline);
+        System.out.println(seperator + greeting + seperator);
+
+        scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            switch (input) {
+                case "list":
+                    list();
+                    break;
+                case "blah":
+                    blah();
+                    break;
+                case "bye":
+                    bye();
+                    System.exit(0);
+                    break;
+                default:
+                    unrecognised();
+                    break;
+            }
+        }
+    }
+
+    private static void list() {
+        String msg = "      list\n";
+        System.out.println(seperator + msg + seperator);
+    }
+
+    private static void blah() {
+        String msg = "      blah\n";
+        System.out.println(seperator + msg + seperator);
+    }
+
+    private static void bye() {
+        String msg = "      Bye. Hope to see you again soon! ☆*:.｡. o(≧▽≦)o .｡.:*☆\n";
+        System.out.println(seperator + msg + seperator);
+    }
+
+    private static void unrecognised() {
+        String msg = "      I don't understand what you said :(\n";
+        System.out.println(seperator + msg + seperator);
     }
 }
