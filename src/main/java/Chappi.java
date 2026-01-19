578168ef478c1e527b10b196d339b7faf28ca117
Yixiannya <ohyixian@gmail.com>
2026-01-19 18:05:28 +0800

    Implemented basic List functionality

--- src/main/java/Chappi.java
+++ src/main/java/Chappi.java
@@ -4,12 +4,17 @@ public class Chappi {
     // Common use strings
     private static String seperator = "     ____________________________________________________________\n";
     private static Scanner scanner;
+    private static String[] stringList;
+    private static int listCount;
 
     public static void main(String[] args) {
         // Messages
         String greeting = "      Hello! I'm Chappi!\n" +
                 "      What can I do for you?\n";
 
+        // Initialisation
+        stringList = new String[100];
+        listCount = 0;
         System.out.println(seperator + greeting + seperator);
 
         scanner = new Scanner(System.in);
@@ -29,15 +34,28 @@ public class Chappi {
                     System.exit(0);
                     break;
                 default:
-                    unrecognised();
+                    addToList(input);
                     break;
             }
         }
     }
 
     private static void list() {
-        String msg = "      list\n";
-        System.out.println(seperator + msg + seperator);
+        String result = seperator;
+
+        for (int i = 0; i < stringList.length; i++) {
+            if (stringList[i] == null) {
+                if (i <= 0) {
+                    result = result.concat("       The list is empty!\n");
+                }
+                break;
+            }
+            int index = i + 1;
+            String msg = "       %d. %s\n";
+            result = result.concat(String.format(msg, index, stringList[i]));
+        }
+
+        System.out.println(result + seperator);
     }
 
     private static void blah() {
@@ -50,8 +68,10 @@ public class Chappi {
         System.out.println(seperator + msg + seperator);
     }
 
-    private static void unrecognised() {
-        String msg = "      I don't understand what you said :(\n";
-        System.out.println(seperator + msg + seperator);
+    private static void addToList(String str) {
+        stringList[listCount] = str;
+        listCount++;
+        String msg = "      added: ";
+        System.out.println(seperator + msg + str + "\n" + seperator);
     }
 }
