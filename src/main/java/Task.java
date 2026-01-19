b5ec5a76d0934f36e8755eab95c63db9b76f732f
Yixiannya <ohyixian@gmail.com>
2026-01-19 19:38:15 +0800

    Added Task class and its functionality

---
+++ src/main/java/Task.java
@@ -0,0 +1,32 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return this.description; // mark done task with X
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String statusIcon = this.getStatusIcon();
+        String str = "[%s] %s";
+        return String.format(str, statusIcon, this.description);
+    }
+}
