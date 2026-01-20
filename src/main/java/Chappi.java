a2970c03f0547f9d9799065d08647852a66f281c
Yixiannya <ohyixian@gmail.com>
2026-01-20 21:58:52 +0800

    Implemented commands to add the new Task classes to list

--- src/main/java/Chappi.java
+++ src/main/java/Chappi.java
@@ -29,23 +29,75 @@ public class Chappi {
                 sayBye();
                 System.exit(0);
             } else if (input.startsWith("mark ")) {
-                int index = Integer.parseInt(input.substring(5));
+                int index = Integer.parseInt(trimPrefix(input, "mark "));
                 markList(index);
             } else if (input.startsWith("unmark ")) {
-                int index = Integer.parseInt(input.substring(7));
+                int index = Integer.parseInt(trimPrefix(input, "unmark "));
                 unmarkList(index);
+            } else if (input.startsWith("todo ")) {
+                addToDo(input);
+            } else if (input.startsWith("deadline ")) {
+                addDeadline(input);
+            } else if (input.startsWith("event ")) {
+                addEvent(input);
             } else {
-                addToList(input);
+                addTask(input);
             }
         }
     }
 
+    private static String trimPrefix(String input, String prefix) {
+        return input.substring(prefix.length());
+    }
 
-    private static void addToList(String str) {
+    private static void addToDo(String input) {
+        String description = trimPrefix(input, "todo ");
+        ToDo todo = new ToDo(description);
+        addToTaskList(todo);
+    }
+
+    private static void addDeadline(String input) {
+        String description = trimPrefix(input, "deadline ");
+        if (description.contains("/by ")) {
+            String[] strings = description.split("/by ");
+            Deadline deadline = new Deadline(strings[0], strings[1]);
+            addToTaskList(deadline);
+        } else {
+            System.out.println(seperator
+                    + "     Invalid format.\n"
+                    + "     Please enter a due date with the '/by' keyword.\n"
+                    + seperator);
+        }
+    }
+
+    private static void addEvent(String input) {
+        String description = trimPrefix(input, "todo ");
+        if (description.contains("/from ") && description.contains("/to ")) {
+            String[] strings = description.split("/from ");
+            String[] dateArray = strings[1].split("/to ");
+            Event event = new Event(strings[0], dateArray[0], dateArray[1]);
+            addToTaskList(event);
+        } else {
+            System.out.println(seperator
+                    + "     Invalid format.\n"
+                    + "     Please enter a start date with the '/from' keyword and an end date with the '/to' keyword.\n"
+                    + seperator);
+        }
+    }
+
+    private static void addTask(String str) {
         Task task = new Task(str);
+        addToTaskList(task);
+    }
+
+    private static void addToTaskList(Task task) {
         taskList.add(task);
-        String msg = "      added: %s\n";
-        System.out.println(seperator + String.format(msg, task.getDescription()) + seperator);
+        String msg = "      Sure, added:\n      %s\n";
+        String listLenMsg = String.format("      There's %d tasks in your list now.\n", taskList.size());
+        System.out.println(seperator
+                + String.format(msg, task)
+                + listLenMsg
+                + seperator);
     }
 
     private static void markList(int index) {
