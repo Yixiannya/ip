2729d4cbebc897505df0d7596c0291ae0a0527b2
Yixiannya <ohyixian@gmail.com>
2026-01-21 14:55:33 +0800

    Adjusted formatting of DukeException String format

--- src/main/java/dukeExceptions/DukeException.java
+++ src/main/java/dukeExceptions/DukeException.java
@@ -1,7 +1,14 @@
 package dukeExceptions;
 
 public class DukeException extends Exception {
+    protected String msg;
+
     public DukeException(String message) {
-        super("     There's been a problem.\n" + "     " + message + "\n");
+        msg = "      There's been a problem.\n" + message + "\n";
+    }
+
+    @Override
+    public String toString() {
+        return msg;
     }
 }
