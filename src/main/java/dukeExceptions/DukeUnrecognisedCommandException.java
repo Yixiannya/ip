4216db158e7bebad6ac799c3c21e86b83072b590
Yixiannya <ohyixian@gmail.com>
2026-01-21 14:54:16 +0800

    Added new exception for unrecognised commands by the chatbot

---
+++ src/main/java/dukeExceptions/DukeUnrecognisedCommandException.java
@@ -0,0 +1,7 @@
+package dukeExceptions;
+
+public class DukeUnrecognisedCommandException extends DukeException {
+    public DukeUnrecognisedCommandException() {
+        super("      I did not recognise that command.");
+    }
+}
