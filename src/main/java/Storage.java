c8c704a3d43ff4f7d5f5b744e73eb57d90741609
Yixiannya <ohyixian@gmail.com>
2026-01-28 18:41:03 +0800

    Move load and parseTask method from chatbot to storage

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,6 +1,5 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Storage {
@@ -11,13 +10,53 @@ public class Storage {
     }
 
     public void save(ArrayList<Task> taskList) throws FileNotFoundException {
-        File saveFile = new File(filePath);
+        File saveFile = new File(this.filePath);
         saveFile.getParentFile().mkdirs();
 
         try (PrintWriter printWriter = new PrintWriter(saveFile)) {
             for (Task t : taskList) {
                 printWriter.write(t.toFileString() + "\n");
             }
+        }
+    }
+
+    public ArrayList<Task> load() throws IOException {
+        ArrayList<Task> savedList = new ArrayList<>();
+        File saveFile = new File(this.filePath);
+
+        if (!saveFile.exists()) {
+            return savedList;
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(saveFile))) {
+            String line = br.readLine();
+            while (line != null) {
+                savedList.add(parseTask(line));
+                line = br.readLine();
+            }
+        }
+
+        return savedList;
+    }
+
+    private Task parseTask(String line) {
+        String[] splitLine = line.split(" \\| ");
+        String taskType = splitLine[0];
+        boolean isDone = splitLine[1].equals("1");
+        String description = splitLine[2];
+
+        switch (taskType) {
+        case "T":
+            return new ToDo(description, isDone);
+        case "D":
+            LocalDate deadlineEndDate = LocalDate.parse(trimPrefix(splitLine[3], "End: "));
+            return new Deadline(description, isDone, deadlineEndDate);
+        case "E":
+            LocalDate eventStartDate = LocalDate.parse(trimPrefix(splitLine[3], "Start: "));
+            LocalDate eventEndDate = LocalDate.parse(trimPrefix(splitLine[4], "End: "));
+            return new Event(description, isDone, eventStartDate, eventEndDate);
+        default:
+            throw new IllegalArgumentException("Unknown task type");
         }
     }
 }
