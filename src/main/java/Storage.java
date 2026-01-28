bcc8eef7b3f9fa6d14282b4d3625af3748e2cd29
Yixiannya <ohyixian@gmail.com>
2026-01-28 18:37:00 +0800

    Move save method from chatbot to Storage

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,7 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 public class Storage {
     private String filePath;
 
     public Storage(String filePath) {
+        this.filePath = filePath;
+    }
 
+    public void save(ArrayList<Task> taskList) throws FileNotFoundException {
+        File saveFile = new File(filePath);
+        saveFile.getParentFile().mkdirs();
+
+        try (PrintWriter printWriter = new PrintWriter(saveFile)) {
+            for (Task t : taskList) {
+                printWriter.write(t.toFileString() + "\n");
+            }
+        }
     }
 }
