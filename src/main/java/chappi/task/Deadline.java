e92893af44c4dc2788d5a90cacff3e17dfc92db2
Yixiannya <ohyixian@gmail.com>
2026-01-29 21:46:46 +0800

    Add JavaDoc comments

--- src/main/java/chappi/task/Deadline.java
+++ src/main/java/chappi/task/Deadline.java
@@ -3,14 +3,36 @@ package chappi.task;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents a task with an end date.
+ * Otherwise, has the same properties
+ * as that of the parent Task class.
+ */
 public class Deadline extends Task {
     protected LocalDate endDate;
 
+    /**
+     * Creates a task with the given description.
+     * Also contains end date which is represented
+     * as a LocalDate object.
+     * Automatically marked as not done.
+     * @param description String describing deadline task.
+     * @param endDate LocalDate representation of the task's deadline.
+     */
     public Deadline(String description, LocalDate endDate) {
         super(description);
         this.endDate = endDate;
     }
 
+    /**
+     * Creates a task with the given description.
+     * Also contains end date which is represented
+     * as a LocalDate object.
+     * Marked as done or not done depending on input.
+     * @param description String describing deadline task.
+     * @param isDone Boolean indicating if task is marked as done.
+     * @param endDate LocalDate representation of the task's deadline.
+     */
     public Deadline(String description, boolean isDone, LocalDate endDate) {
         super(description, isDone);
         this.endDate = endDate;
