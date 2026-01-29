24f6687f68476ffdd3ce8b6eaf6a2894871b0e41
Yixiannya <ohyixian@gmail.com>
2026-01-29 21:01:15 +0800

    Add JavaDoc comments

--- src/main/java/chappi/util/Util.java
+++ src/main/java/chappi/util/Util.java
@@ -1,8 +1,19 @@
 package chappi.util;
 
+/**
+ * Contains all utility functions that do not belong to any other class.
+ * Methods are to be used anywhere in Chappi's classes.
+ */
 public class Util {
     public Util() {}
 
+    /**
+     * Removes the specified prefix of a given String.
+     *
+     * @param input Input String to be trimmed.
+     * @param prefix Prefix String to be removed from the input.
+     * @return Trimmed input string.
+     */
     public static String trimPrefix(String input, String prefix) {
         return input.substring(prefix.length());
     }
